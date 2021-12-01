cf5810cf1f0f398b1ce0afcc1382ac9f77f329a2
666JACK999 <L16340614@nogales.tecnm.mx>
2021-12-01 14:19:57 -0700

    se agrega el proyecto

---
+++ acercade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Barber_shop</title>
+        <meta name="author" content="iThemesLab">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="shortcut icon" href="img/logo.png">
+        <link rel="apple-touch-icon" href="assets/favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="assets/favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="assets/favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="assets/favicon/apple-icon-144x144.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suscríbete a Devbro</title>
+    <link rel="stylesheet" href="./stylead.css">
+</head>
+<body>
+    <!-- Primera Sección -->
+    <div id="primera-seccion">
+        <div id="barra-navegacion">
+            <p id="logo">CUEVAS <span style="color: #fb2056;">BARBERIA</span></p>
+            <nav>
+                <a href="barber_shop.php">Inicio</a>
+                <a href="servicios.php">Servicios</a>
+                <a href="contacto.php">Contacto</a>
+            </nav>
+        </div>
+
+        <div id="primera-seccion-contenido">
+            <p>¿necesitas un corte?</p>
+            <hr>
+            <h1>Tenemos los mejores servicios</h1>
+            <a href="#" class="btn-rojo">más información</a>
+        </div>
+    </div>
+
+    <!-- Segunda Sección -->
+  
+
+    <!-- Tercera Sección -->
+    <div id="tercera-seccion">
+        <div id="titulo">
+            <h2>Contamos Con Todas Las Restricciones</h2>
+            <hr>
+        </div>
+        <div id="container-tercera-seccion">
+            <div class="img-tercera">
+                <img src="./assets/tercera_seccion/restricciones.jpg" alt="">
+               
+            </div>
+            <div class="img-tercera">
+                <img src="./assets/tercera_seccion/restriciones2.jpg" alt="">
+               
+            </div>
+        </div>
+    </div>
+
+    <!-- Cuarta Sección -->
+    <div id="cuarta-seccion">
+        <div id="contenedor">
+            <div id="cuarta-seccion-izq" class="mitad"></div>
+            <div id="cuarta-seccion-der" class="mitad">
+                <h3>Explora Nuestros Servicios</h3>
+                <hr>
+                <p>
+                    Contamos con los mejores servicios que una barberia te puede ofrecer
+                </p>
+                <a href="servicios.php" class="btn-rojo">Más información</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Quinta Sección -->
+    <div id="quinta-seccion">
+        <div id="quinta-seccion-izquierda">
+            <h3>Los Mejores Cortes</h3>
+            <hr>
+            <p>
+                Consulta nuestros cortes y precios para diferentes servicios, no tienes excusa para no verte bien.
+                reserva una cita ya!!!!!
+            </p>
+            <a href="citas.php" class="btn-rojo">Más información</a>
+        </div>
+        <div id="quinta-seccion-derecha">
+            <img src="./assets/quinta_seccion/corte1.jpg" alt="">
+            <img src="./assets/quinta_seccion/corte2.jpg" alt="">
+            <img src="./assets/quinta_seccion/corte3.jpg" alt="">
+            <img src="./assets/quinta_seccion/corte4.jpg" alt="">
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <footer>
+        <p>Avenida Tecnologico #237 Local 1 Colonia La Granja, Heroica Nogales, 84065 | Tel: 631-123-4567 | Email: 16340614@itnogales.edu.mx</p>
+        <p>Horario de atencion de 10 a.m. a 8p.m <span style="color: #fb2056;">Barberia Cuevas</span></p>
+    </footer>
+
+</body>
+</html>
---
+++ servicios.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+     <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Barber_shop</title>
+        <meta name="author" content="iThemesLab">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="shortcut icon" href="img/logo.png">
+        <link rel="apple-touch-icon" href="assets/favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="assets/favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="assets/favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="assets/favicon/apple-icon-144x144.png">
+ 
+  <link rel="stylesheet" href="./style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700%7cPlayfair+Display:400,700" rel="stylesheet">
+        
+        <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+        <link rel="stylesheet" href="imagen.css">
+        
+        <link rel="stylesheet" href="assets/css/vendor/rev_slider/settings.css">
+        <link rel="stylesheet" href="assets/css/vendor/rev_slider/navigation.css">
+  
+        <link rel="stylesheet" href="assets/css/vendor/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/fonts/flaticon/flaticon.css">
+  
+        <link rel="stylesheet" href="assets/css/vendor/owl.carousel.css">
+        <link rel="stylesheet" href="assets/css/vendor/owl.theme.css">
+        
+        <link rel="stylesheet" href="assets/css/vendor/bootstrap-datepicker.standalone.css">
+  
+        <link rel="stylesheet" href="assets/css/style.css">
+ 
+        <link rel="stylesheet" href="assets/css/responsive.css">
+        <div class="header navbar-fixed-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <nav class="navbar navbar-default">
+                            <div class="container-fluid">
+                                
+                                <div class="navbar-header">
+                                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                        <span class="sr-only">Toggle navigation</span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                    </button>
+                                    <a class="navbar-brand" href="index-2.html">
+                                        <img src="img/cuevas.jpg" width="80" alt="">
+                                    </a>
+                                </div> 
+
+                                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                                    <div class="top-right-contact text-right pull-right hidden-sm hidden-xs">
+                                        <i class="fa fa-phone-square"></i> (+52) 6311895500)
+                                    </div> <!--top-right-contact-->
+                                    <ul class="nav navbar-nav">
+                                        <li class="active"><a href="barber_shop.php">Inicio</a></li>
+                                        <li><a href="acercade.php">Acerca de</a></li>
+                                        <li><a href="servicios.php">Servicios</a></li>
+                                        <li><a href="citas.php">Reservas</a></li>
+                                        <li><a href="contacto.php">Contacto</a></li>
+                                        <li><a href="index.php"><font color="red">SALIR</font></a></li>
+                                    </ul>
+                                    
+                                </div>
+                            </div> 
+                        </nav>
+
+                    </div> 
+                    
+                </div>
+            </div> 
+        </div>                    
+</head>
+
+<body >
+   
+ <section id="slide">
+                <img src="img/bg-3.jpg" width="1500" height="500">
+            </section>
+<!-- partial:index.partial.html -->
+<div class="conte ">
+         <div class="card stromtroper">
+             <img src="https://scontent.fhmo2-1.fna.fbcdn.net/v/t1.6435-9/72265594_133850007984259_374452806691586048_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=e3f864&_nc_eui2=AeHhaxvrNEbqD5FOupA8Ivo7cUU4etWFEzpxRTh61YUTOq30rFn1BkuUm3qzx_2htnXCVeDr3UM7Fzj-T3bVeIdY&_nc_ohc=bBw4uaQOlJEAX9tAiyc&_nc_ht=scontent.fhmo2-1.fna&oh=4079ab157a074b853de87de6fe71d710&oe=60FC55E8" alt="">
+         </div>
+         <div class="informacion">
+             <h1>Membresia De Cortes</h1>
+             <h1>       ㅤ    </h1>
+            <p class="fecha">
+                A Partir De 27/06/2021
+            </p>
+         </div>
+         <div class="precio">
+             <div class="box-precio">
+                 <span class="precio1"><strike>S/250.00</strike> </span>
+             <span class="precio2"><b>S/150.00</b></span>
+             </div>
+             <span class="shoping"><i class="fas fa-cart-plus"></i></span>
+         </div>
+        
+             <button>comprar</button>
+         
+     </div>
+     
+    <div class="conte">
+         <div class="card iron-man">
+             <img src="https://scontent.fhmo2-2.fna.fbcdn.net/v/t1.6435-9/135914563_447538249948765_1901427565804792211_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=8bfeb9&_nc_eui2=AeFs_9Vqr830OpMu5_bxeuWXLI9uJHot2H8sj24kei3Yf5MMA2R1Z31K7VE1OghtRQKoa1w9VZMmcmyisrwTKkLX&_nc_ohc=YoONsWMkpocAX9h-axK&_nc_ht=scontent.fhmo2-2.fna&oh=43e01dcd9eeccd5efc0f7134fe5a225e&oe=60FB2E3B" alt="">
+         </div>
+         <div class="informacion">
+             <h1>Corte Con Descuento</h1>
+             <h1>ㅤ</h1>
+            <p class="fecha">
+                Todos Los Miercoles
+            </p>
+         </div>
+         <div class="precio">
+             <div class="box-precio">
+                 <span class="precio1"><strike>S/100.00</strike> </span>
+                 <span class="precio2"><b>S/60.00</b></span>
+             </div>
+             <span class="shoping"><i class="fas fa-cart-plus"></i></span>
+         </div>
+         <button>comprar</button>
+     </div>
+     
+    <div class="conte">
+         <div class="card spider-man">
+             <img src="https://scontent.fhmo2-1.fna.fbcdn.net/v/t1.6435-9/168824212_504082697627653_2171624542007184847_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=8bfeb9&_nc_eui2=AeESo6lKzafTb7z5fAiI4dGcXPzenD_ANplc_N6cP8A2mYlDq9Qj1_OkCeyK5_Dp552sYY82JUSiMXJ6azIxFawy&_nc_ohc=S2Ywr8v6GmIAX_UFRh2&_nc_ht=scontent.fhmo2-1.fna&oh=44b24bcd73e9faf53355fd32cd6527ea&oe=60FBCCA7" alt="">
+         </div>
+         <div class="informacion">
+             <h1>Mascarilla</h1>
+             <h1>ㅤ</h1>
+            <p class="fecha">
+                A Partir de 27/06/2021
+            </p>
+         </div>
+         <div class="precio">
+             <div class="box-precio">
+                 <span class="precio1"><strike>S/100.00</strike> </span>
+             <span class="precio2"><b>S/50.00</b></span>
+             </div>
+             <span class="shoping"><i class="fas fa-cart-plus"></i></span>
+         </div>
+         <button>comprar</button>
+     </div>
+<!-- partial -->
+  
+</body>
+
+</html>
